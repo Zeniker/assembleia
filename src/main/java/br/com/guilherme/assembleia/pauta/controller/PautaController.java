940a3e550669dcb4d9052b2be484e902022e8bb8
Guilherme Lopes <gui.lls@hotmail.com>
2019-09-14 18:49:42 -0300

    Adicionado requestMapping em PautaController.

--- src/main/java/br/com/guilherme/assembleia/pauta/controller/PautaController.java
+++ src/main/java/br/com/guilherme/assembleia/pauta/controller/PautaController.java
@@ -7,11 +7,13 @@ import br.com.guilherme.assembleia.pauta.model.Pauta;
 import br.com.guilherme.assembleia.pauta.service.PautaService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@RestController("pauta")
+@RestController
+@RequestMapping("pauta")
 public class PautaController {
 
     private PautaService pautaService;
