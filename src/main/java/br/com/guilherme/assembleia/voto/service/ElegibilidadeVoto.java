e11bfce166a8b2a2e0fc02b75b2b43020909b26b
Guilherme Lopes <gui.lls@hotmail.com>
2019-09-16 20:43:26 -0300

    Alterada a classe ElegibilidadeVoto para se tornar uma interface. Criada a implementação ElegibilidadeVotoHttp, onde irá consultar um serviço externo. Criada a implementação ElegibilidadeVotoTest, que sempre irá retornar ABLE_TO_VOTE. Criada funcionalidade para carregar a implementação de ElegibilidadeVoto de acordo com o profile que estiver ativo.

--- src/main/java/br/com/guilherme/assembleia/voto/service/ElegibilidadeVoto.java
+++ src/main/java/br/com/guilherme/assembleia/voto/service/ElegibilidadeVoto.java
@@ -1,24 +1,15 @@
 package br.com.guilherme.assembleia.voto.service;
 
 import br.com.guilherme.assembleia.voto.dto.CPFStatusDTO;
-import br.com.guilherme.assembleia.voto.exception.CPFInvalidoException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 /**
  * Classe para verificar a elegibilidade de voto
  *
  * @author
  */
-@Component
-public class ElegibilidadeVoto {
+public interface ElegibilidadeVoto {
 
     /**
      * Verifica se o cpf informado está elegível para voto e retorna seu status
@@ -26,23 +17,5 @@ public class ElegibilidadeVoto {
      * @param cpf CPF a ser verificado
      * @return Status do cpf
      */
-    public CPFStatusDTO associadoPodeVotar(String cpf){
-
-        try{
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-
-            HttpGet get = new HttpGet("https://user-info.herokuapp.com/users/" + cpf);
-
-            HttpResponse httpResponse = httpClient.execute(get);
-
-            if(httpResponse.getStatusLine().getStatusCode() == 404) throw new CPFInvalidoException();
-
-            String responseBody = EntityUtils.toString(httpResponse.getEntity());
-
-            return new ObjectMapper().readValue(responseBody, CPFStatusDTO.class);
-
-        }catch (IOException e){
-            throw new RuntimeException("Não foi possível consultar o CPF do associado");
-        }
-    }
+    CPFStatusDTO associadoPodeVotar(String cpf);
 }
--- src/test/java/br/com/guilherme/assembleia/voto/service/ElegibilidadeVotoHttpIT.java
+++ src/test/java/br/com/guilherme/assembleia/voto/service/ElegibilidadeVotoHttpIT.java
@@ -1,22 +1,21 @@
 package br.com.guilherme.assembleia.voto.service;
 
+import br.com.guilherme.assembleia.TesteIntegracao;
 import br.com.guilherme.assembleia.voto.dto.CPFStatusDTO;
 import br.com.guilherme.assembleia.voto.exception.CPFInvalidoException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ElegibilidadeVotoIT {
+@SpringBootTest
+class ElegibilidadeVotoHttpIT implements TesteIntegracao {
 
+    @Autowired
     private ElegibilidadeVoto elegibilidadeVoto;
-
-    @BeforeEach
-    void setUp() {
-        elegibilidadeVoto = new ElegibilidadeVoto();
-    }
 
     @DisplayName("Teste associado pode votar")
     @Test
